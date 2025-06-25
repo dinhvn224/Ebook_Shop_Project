35c862e1052ee3edc79421e0ab98d20714cb357a
ptd98 <ducptph52678@gmail.com>
2025-06-26 00:00:00 +0700

    giao dien

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,24 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
-use App\Http\Controllers\Admin\AuthorController;
-use App\Http\Controllers\Admin\PublisherController;
-use App\Http\Controllers\Admin\CategoryController;
-Route::prefix('admin')->name('admin.')->group(function() {
-    Route::resource('authors', AuthorController::class)->except(['show']);
-    Route::post('authors/{id}/restore', [AuthorController::class, 'restore'])->name('authors.restore');
-});
+
+// Trang chủ cho tất cả mọi người
+Route::get('/', function () {
+    return view('client.home');
+})->name('home');
+
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-
+// Yêu cầu đăng nhập
 Route::middleware(['auth', 'role:user'])->get('/home', function () {
     return view('client.home');
-})->name('home');
+})->name('home.user');
+
 
 Route::middleware(['auth', 'role:admin'])->get('/admin', function () {
     return view('admin.dashboard');
