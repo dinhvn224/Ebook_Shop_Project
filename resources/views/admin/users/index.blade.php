63c00b68bdabe4afded628e5357e554283ba8aef
NgocHan8 <ng.han20005@gmail.com>
2025-06-26 23:16:43 +0700

    sửa gd user

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,13 +1,9 @@
 @extends('admin.layouts.app')
 
 @section('content')
-<h1>Danh sách Người Dùng</h1>
+<!-- <h1>Danh sách Người Dùng</h1>
 
-<a href="{{ route('admin.users.create') }}" class="btn btn-primary mb-3">Thêm mới người dùng</a>
-
-@if(session('success'))
-    <div class="alert alert-success">{{ session('success') }}</div>
-@endif
+<a href="" class="btn btn-primary mb-3">Thêm mới người dùng</a>
 
 <table class="table table-bordered table-striped">
     <thead class="thead-dark">
@@ -24,9 +20,9 @@
     <tbody>
         @foreach($users as $user)
         <tr>
-            <td>{{ $user->id }}</td>
-            <td>{{ $user->name }}</td>
-            <td>{{ $user->email }}</td>
+            <td></td>
+            <td></td>
+            <td></td>
             <td>
                 @if($user->avatar_url)
                     <img src="{{ asset('storage/' . $user->avatar_url) }}" alt="Avatar" width="50" height="50">
@@ -34,21 +30,339 @@
                     <span>Chưa có ảnh</span>
                 @endif
             </td>
-            <td>{{ $user->birth_date ? $user->birth_date->format('d-m-Y') : 'Chưa cập nhật' }}</td>
-            <td>{{ $user->is_active ? 'Active' : 'Inactive' }}</td>
+            <td></td>
+            <td></td>
             <td>
-                <a href="{{ route('admin.users.edit', $user->id) }}" class="btn btn-warning btn-sm">Sửa</a>
-                <form action="{{ route('admin.users.destroy', $user->id) }}" method="POST" style="display:inline-block;">
-                    @csrf
-                    @method('DELETE')
-                    <button class="btn btn-danger btn-sm" onclick="return confirm('Bạn có chắc muốn xóa?')">Xóa</button>
-                </form>
+                <a href="" class="btn btn-warning btn-sm">Sửa</a>
+
             </td>
         </tr>
         @endforeach
     </tbody>
-</table>
+</table> -->
 
-{{ $users->links() }}  <!-- Phân trang -->
+<!-- {{ $users->links() }}  Phân trang -->
+
+    <div id="global-loader">
+        <div class="whirly-loader"> </div>
+    </div>
+
+    <div class="main-wrapper">
+        <div class="page-wrapper">
+            <div class="content">
+                <div class="page-header">
+                    <div class="page-title">
+                        <h4>Danh sách người dùng</h4>
+                        <h6>Quản lý người dùng của bạn</h6>
+                    </div>
+                    <div class="page-btn">
+                        <a href="{{ route('admin.users.create') }}" class="btn btn-added"><img src="{{ asset('assets/img/icons/plus.svg') }}" alt="img">Thêm
+                            người dùng</a>
+                    </div>
+                </div>
+                @if(session('success'))
+                    <div class="alert alert-success">{{ session('success') }}</div>
+                @endif
+                <div class="card">
+                    <div class="card-body">
+                        <div class="table-top">
+                            <div class="search-set">
+                                <div class="search-path">
+                                    <a class="btn btn-filter" id="filter_search">
+                                        <img src="assets/img/icons/filter.svg" alt="img">
+                                        <span><img src="assets/img/icons/closes.svg" alt="img"></span>
+                                    </a>
+                                </div>
+                                <div class="search-input">
+                                    <a class="btn btn-searchset"><img src="assets/img/icons/search-white.svg"
+                                            alt="img"></a>
+                                    <div id="DataTables_Table_0_filter" class="dataTables_filter"><label> <input
+                                                type="search" class="form-control form-control-sm"
+                                                placeholder="Tìm kiếm" aria-controls="DataTables_Table_0"></label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card" id="filter_inputs">
+                            <div class="card-body pb-0">
+                                <div class="row">
+                                    <div class="col-lg-2 col-sm-6 col-12">
+                                        <div class="form-group">
+                                            <input type="text" placeholder="Tên người dùng">
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-2 col-sm-6 col-12">
+                                        <div class="form-group">
+                                            <input type="text" placeholder="Số điện thoại">
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-2 col-sm-6 col-12">
+                                        <div class="form-group">
+                                            <input type="text" placeholder="Email">
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-2 col-sm-6 col-12">
+                                        <div class="form-group">
+                                            <input type="text" class="datetimepicker cal-icon" placeholder="Chọn ngày">
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-2 col-sm-6 col-12">
+                                        <div class="form-group">
+                                            <select class="select">
+                                                <option>Vô hiệu hóa</option>
+                                                <option>Kích hoạt</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-1 col-sm-6 col-12 ms-auto">
+                                        <div class="form-group">
+                                            <a class="btn btn-filters ms-auto"><img
+                                                    src="assets/img/icons/search-whites.svg" alt="img"></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="table-responsive">
+                            <table class="table  datanew">
+                                <thead>
+                                    <tr>
+                                        <th>
+                                            <label class="checkboxs">
+                                                <input type="checkbox">
+                                                <span class="checkmarks"></span>
+                                            </label>
+                                        </th>
+                                        <th>Tên người dùng</th>
+                                        <th>Email</th>
+                                        <th>Số điện thoại</th>
+                                        <th>Ngày sinh</th>
+                                        <th>Vai trò</th>
+                                        <th>Trạng thái</th>
+                                        <th>Hành động</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach($users as $user)
+                                    <tr>
+                                        <td>
+                                            <label class="checkboxs">
+                                                <input type="checkbox">
+                                                <span class="checkmarks"></span>
+                                            </label>
+                                        </td>
+                                        <td>{{ $user->name }}</td>
+                                        <td>{{ $user->email }}</td>
+                                        <td>{{ $user->phone_number }}</td>
+                                        <td>{{ $user->role }}</td>
+                                        <td>{{ $user->birth_date ? $user->birth_date->format('d-m-Y') : 'Chưa cập nhật' }}</td>
+                                        <td>
+                                            <span class="bg-lightgreen badges bg-lightred badges">{{ $user->is_active ? 'Active' : 'Inactive' }}</span>
+                                        </td>
+                                        <td>
+                                            <a class="me-3" href="{{ route('admin.users.edit', $user->id) }}">
+                                                <img src="assets/img/icons/edit.svg" alt="img">
+                                            </a>
+                                            <form action="{{ route('admin.users.destroy', $user->id) }}" method="POST">
+                                                @csrf
+                                                @method('DELETE')
+                                            <a class="me-3 confirm-text" href="javascript:void(0);" onclick="return confirm('Bạn có chắc muốn xóa?')">
+                                                <img src="assets/img/icons/delete.svg" alt="img">
+                                            </a>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                    @endforeach
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+
+    <div class="modal fade" id="showpayment" tabindex="-1" aria-labelledby="showpayment" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Xem Thanh Toán</h5>
+                    <button type="button" class="close" data-bs-dismiss="modal" aria-label="Đóng"><span
+                            aria-hidden="true">×</span></button>
+                </div>
+                <div class="modal-body">
+                    <div class="table-responsive">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>Ngày</th>
+                                    <th>Tham chiếu</th>
+                                    <th>Số tiền</th>
+                                    <th>Thanh toán bởi</th>
+                                    <th>Hành động</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr class="bor-b1">
+                                    <td>2022-03-07</td>
+                                    <td>INV/SL0101</td>
+                                    <td>$ 1500.00</td>
+                                    <td>Tiền mặt</td>
+                                    <td>
+                                        <a class="me-2" href="javascript:void(0);">
+                                            <img src="assets/img/icons/printer.svg" alt="img">
+                                        </a>
+                                        <a class="me-2" href="javascript:void(0);" data-bs-target="#editpayment"
+                                            data-bs-toggle="modal" data-bs-dismiss="modal">
+                                            <img src="assets/img/icons/edit.svg" alt="img">
+                                        </a>
+                                        <a class="me-2 confirm-text" href="javascript:void(0);">
+                                            <img src="assets/img/icons/delete.svg" alt="img">
+                                        </a>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="createpayment" tabindex="-1" aria-labelledby="createpayment" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Tạo Thanh Toán</h5>
+                    <button type="button" class="close" data-bs-dismiss="modal" aria-label="Đóng"><span
+                            aria-hidden="true">×</span></button>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-lg-6 col-sm-12 col-12">
+                            <div class="form-group">
+                                <label>Khách hàng</label>
+                                <div class="input-group">
+                                    <input type="text" value="2022-03-07" class="datetimepicker">
+                                    <a class="scanner-set input-group-text">
+                                        <img src="assets/img/icons/datepicker.svg" alt="img">
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-sm-12 col-12">
+                            <div class="form-group">
+                                <label>Tham chiếu</label>
+                                <input type="text" value="INV/SL0101">
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-sm-12 col-12">
+                            <div class="form-group">
+                                <label>Số tiền nhận</label>
+                                <input type="text" value="1500.00">
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-sm-12 col-12">
+                            <div class="form-group">
+                                <label>Số tiền thanh toán</label>
+                                <input type="text" value="1500.00">
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-sm-12 col-12">
+                            <div class="form-group">
+                                <label>Hình thức thanh toán</label>
+                                <select class="select">
+                                    <option>Tiền mặt</option>
+                                    <option>Trực tuyến</option>
+                                    <option>Đang xử lý</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-lg-12">
+                            <div class="form-group">
+                                <label>Ghi chú</label>
+                                <textarea class="form-control"></textarea>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-submit">Gửi</button>
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Đóng</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="editpayment" tabindex="-1" aria-labelledby="editpayment" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Chỉnh Sửa Thanh Toán</h5>
+                    <button type="button" class="close" data-bs-dismiss="modal" aria-label="Đóng"><span
+                            aria-hidden="true">×</span></button>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-lg-6 col-sm-12 col-12">
+                            <div class="form-group">
+                                <label>Khách hàng</label>
+                                <div class="input-group">
+                                    <input type="text" value="2022-03-07" class="datetimepicker">
+                                    <a class="scanner-set input-group-text">
+                                        <img src="assets/img/icons/datepicker.svg" alt="img">
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-sm-12 col-12">
+                            <div class="form-group">
+                                <label>Tham chiếu</label>
+                                <input type="text" value="INV/SL0101">
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-sm-12 col-12">
+                            <div class="form-group">
+                                <label>Số tiền nhận</label>
+                                <input type="text" value="1500.00">
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-sm-12 col-12">
+                            <div class="form-group">
+                                <label>Số tiền thanh toán</label>
+                                <input type="text" value="1500.00">
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-sm-12 col-12">
+                            <div class="form-group">
+                                <label>Hình thức thanh toán</label>
+                                <select class="select">
+                                    <option>Tiền mặt</option>
+                                    <option>Trực tuyến</option>
+                                    <option>Đang xử lý</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-lg-12">
+                            <div class="form-group">
+                                <label>Ghi chú</label>
+                                <textarea class="form-control"></textarea>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-submit">Gửi</button>
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Đóng</button>
+                </div>
+            </div>
+        </div>
+    </div>
 
 @endsection
+
